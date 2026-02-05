6fc2ef498a50225220945a1125f6f84a9f7f64d5
Vladyslav Stasiuk <cheshire.cat.universe@gmail.com>
2026-02-05 21:49:23 +0200

    [Create] Schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,13 @@
+from .movie import (  # noqa: F401
+    GenreCreate,
+    GenreRead,
+    StarCreate,
+    StarRead,
+    DirectorCreate,
+    DirectorRead,
+    CertificationCreate,
+    CertificationRead,
+    MovieCreate,
+    MovieUpdate,
+    MovieRead,
+)

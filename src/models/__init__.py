39d2575cf6e0b5b5458546fb37b7ed30b4e852f0
Vadym Vynokur <king.krollie@gmail.com>
2026-02-06 18:05:37 +0200

    fix: migrations

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,14 @@
 from .payments import Payment
 from .payment_items import PaymentItem
 from .order import Order, OrderItem
+from .cart import Cart, CartItem
 
 __all__ = [
     Payment,
     PaymentItem,
     Order,
-    OrderItem
+    OrderItem,
+    Cart,
+    CartItem,
 ]
 

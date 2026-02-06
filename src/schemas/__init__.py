e638fe34b76931d1da2c11a768a9606ae7833775
Andrii Chychkan <mywork1728@gmail.com>
2026-02-07 00:04:57 +0200

    chore: added order schemas into init imports

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .payments import PaymentCreateSchema
 from .payment_items import PaymentCreateItemSchema
+from .order import OrderItemCreateSchema, OrderCreateSchema
 
 __all__ = [PaymentCreateSchema, PaymentCreateItemSchema]
 from .movie import (  # noqa: F401

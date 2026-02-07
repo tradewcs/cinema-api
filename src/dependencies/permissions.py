07d931345192c775ae42b980569cb410f5fb298c
Vladyslav Stasiuk <cheshire.cat.universe@gmail.com>
2026-02-07 12:19:15 +0200

    [Add] Permissions

--- src/dependencies/permissions.py
+++ src/dependencies/permissions.py
@@ -1,11 +1,11 @@
-from fastapi import Request
+from fastapi import HTTPException, Request
+from src.enums.accounts import UserGroupEnum
 
 
 async def require_moderator(request: Request) -> None:
-    """
-    TODO (future logic):
-    - retrieve user from request.state.user or through JWT
-    - check role MODERATOR/ADMIN
-    - or 403
-    """
-    return None
+    user = getattr(request.state, "user", None)
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+    if user.group.name not in (UserGroupEnum.MODERATOR, UserGroupEnum.ADMIN):
+        raise HTTPException(status_code=403, detail="Moderator privileges required")

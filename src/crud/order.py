6ebdd00660587e741136ed4b6d0569ac3b20f147
Andrii Chychkan <mywork1728@gmail.com>
2026-02-08 00:27:49 +0200

    feat: added delete and get_by_id

--- src/crud/order.py
+++ src/crud/order.py
@@ -14,7 +14,14 @@ class CrudOrderItem:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    def create_order_item(self, order_id: int, movie_id: int, price: int) -> OrderItem:
+    async def get_order_item_by_id(self, order_item_id: int) -> OrderItem | None:
+        stmt = select(OrderItem).where(OrderItem.id == order_item_id)
+
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+
+
+    async def create_order_item(self, order_id: int, movie_id: int, price: int) -> OrderItem:
         db_order_item = OrderItem(
             order_id=order_id,
             movie_id=movie_id,
@@ -22,13 +29,22 @@ class CrudOrderItem:
         )
 
         self.db.add(db_order_item)
-        self.db.commit()
-        self.db.refresh(db_order_item)
+        await self.db.commit()
+        await self.db.refresh(db_order_item)
         return db_order_item
 
-    async def get_order_by_order_item(self, order_id: int):
+    async def get_order_by_order_item(self, order_id: int) -> Order:
         stmt = select(Order).where(Order.id == order_id)
 
         result = await self.db.execute(stmt)
-        order = result.scalar_one_or_none()
-        return OrderReadSchema.model_validate(order)
+        return result.scalar_one_or_none()
+
+    async def delete_order_item(self, order_item_id: int) -> bool:
+        db_order_item = await self.get_order_item_by_id(order_item_id=order_item_id)
+
+        if not db_order_item:
+            return False
+
+        await self.db.delete(db_order_item)
+        await self.db.commit()
+        return True

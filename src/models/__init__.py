2784e24228c6e4f987d21d583ea446b72711a9a3
Taras Ivashchuk <tvishy@disroot.org>
2026-02-08 22:03:10 +0200

    fix: __init__.py for models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,4 @@
 from .payments import Payment, PaymentItem
 from .order import Order, OrderItem
 
-__all__ = [
-    Payment,
-    PaymentItem,
-    Order,
-    OrderItem
-]
-
+__all__ = [Payment, PaymentItem, Order, OrderItem]

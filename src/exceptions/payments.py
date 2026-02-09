d0ad0ee10e2d03130ca9c57198259401ad089f3c
Taras Ivashchuk <tvishy@disroot.org>
2026-02-09 13:36:06 +0200

    fead: add Payment exceptions

---
+++ src/exceptions/payments.py
@@ -0,0 +1,43 @@
+class PaymentError(Exception):
+    pass
+
+
+# general
+
+
+class PaymentDoesNotExist(PaymentError):
+    pass
+
+
+# create session error
+
+
+class PaymentSessionError(PaymentError):
+    pass
+
+
+class PaymentAmountMismatch(PaymentSessionError):
+    pass
+
+
+class PaymentNotAllowed(PaymentSessionError):
+    pass
+
+
+# webhook error
+
+
+class WebHookPaymentError(PaymentError):
+    pass
+
+
+class InvalidPayload(WebHookPaymentError):
+    pass
+
+
+class InvalidSignature(WebHookPaymentError):
+    pass
+
+
+class SessionDoesNotExistError(WebHookPaymentError):
+    pass

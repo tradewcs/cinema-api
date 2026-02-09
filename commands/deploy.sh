06e1c7ffe3872d27d0dab89f236269c7912cb54e
Stanislav Kishechko <skishechko@gmail.com>
2026-02-09 14:43:14 +0200

    CI/CD

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -10,22 +10,22 @@ handle_error() {
 }
 
 # Navigate to the application directory
-cd /home/ubuntu/src/py-fastapi-homework-5-ec2-deploy-task || handle_error "Failed to navigate to the application directory."
+cd /home/ubuntu/src/cinema-api|| handle_error "Failed to navigate to the application directory."
 
 # Fetch the latest changes from the remote repository
 echo "Fetching the latest changes from the remote repository..."
-git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+git fetch origin devops || handle_error "Failed to fetch updates from the 'origin' remote."
 
 # Reset the local repository to match the remote 'main' branch
-echo "Resetting the local repository to match 'origin/main'..."
-git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+echo "Resetting the local repository to match 'origin/devops'..."
+git reset --hard origin/devops || handle_error "Failed to reset the local repository to 'origin/devops'."
 
 # (Optional) Pull any new tags from the remote repository
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
 # Build and run Docker containers with Docker Compose v2
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+docker compose -f docker/docker-compose.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose.yml."
 
 # Print a success message upon successful deployment
 echo "Deployment completed successfully."
